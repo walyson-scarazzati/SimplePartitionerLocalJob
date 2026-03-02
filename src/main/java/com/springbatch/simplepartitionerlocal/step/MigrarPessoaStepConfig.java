0e510e1172bba071136e3819b36e947e79acf353
Walyson Scarazzati da Silva <walyson.scarazzati@gmail.com>
2026-03-02 18:33:32 +0000

    first commit

---
+++ src/main/java/com/springbatch/simplepartitionerlocal/step/MigrarPessoaStepConfig.java
@@ -0,0 +1,37 @@
+package com.springbatch.simplepartitionerlocal.step;
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.springbatch.simplepartitionerlocal.dominio.Pessoa;
+
+@Configuration
+public class MigrarPessoaStepConfig {
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+	
+	@Autowired
+	@Qualifier("transactionManagerApp")
+	private PlatformTransactionManager transactionManagerApp;
+	
+	@Bean
+	public Step migrarPessoaStep(
+			ItemReader<Pessoa> arquivoPessoaReader,
+			ItemWriter<Pessoa> pessoaWriter) {
+		return stepBuilderFactory
+				.get("migrarPessoaStep")
+				.<Pessoa, Pessoa>chunk(20000)
+				.reader(arquivoPessoaReader)
+				.writer(pessoaWriter)
+				.transactionManager(transactionManagerApp)
+				.build();
+	}
+
+}
---
+++ src/test/java/com/springbatch/simplepartitionerlocaljob/SimplePartitionerLocalJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbatch.simplepartitionerlocaljob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePartitionerLocalJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
